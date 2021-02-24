2e432b38a9dbfe96bd5ea7b469ad1c4a7b5f7dfe
Armand Philippot <git@armandphilippot.com>
2021-02-24 17:57:05 +0100

    chore: enable Gutenberg for blog page
    Thanks to the hook, it is now possible to add text on blog page and
    display it as an introduction.

--- inc/hooks.php
+++ inc/hooks.php
@@ -264,3 +264,21 @@ function apcom_comment_form_submit_button( $button ) {
 	return $button;
 }
 add_filter( 'comment_form_submit_button', 'apcom_comment_form_submit_button' );
+
+/**
+ * Simulate non-empty content to enable Gutenberg editor on Blog page.
+ *
+ * @see https://wordpress.stackexchange.com/a/350563
+ *
+ * @param bool    $replace Whether to replace the editor.
+ * @param WP_Post $post    Post object.
+ * @return bool
+ */
+function apcom_enable_gutenberg_editor_for_blog_page( $replace, $post ) {
+	if ( ! $replace && absint( get_option( 'page_for_posts' ) ) === $post->ID && empty( $post->post_content ) ) {
+		// This comment will be removed by Gutenberg since it won't parse into block.
+		$post->post_content = '<!--non-empty-content-->';
+	}
+	return $replace;
+}
+add_filter( 'replace_editor', 'apcom_enable_gutenberg_editor_for_blog_page', 10, 2 );
--- index.php
+++ index.php
@@ -40,6 +40,15 @@ if ( have_posts() ) {
 						</dd>
 					</div>
 				</dl>
+				<?php
+			}
+			$apcom_page_for_posts_id      = get_option( 'page_for_posts' );
+			$apcom_page_for_posts_content = get_the_content( null, false, $apcom_page_for_posts_id );
+			if ( '' !== $apcom_page_for_posts_content ) {
+				?>
+				<div class="page__introduction">
+					<?php echo wp_kses_post( $apcom_page_for_posts_content ); ?>
+				</div>
 			<?php } ?>
 		</header>
 		<div class="page__body" itemprop="articleBody">
