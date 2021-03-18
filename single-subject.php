0f91f09d7e3bb158abf55862c5dc01d12413d5eb
Armand Philippot <git@armandphilippot.com>
2021-03-18 17:44:06 +0100

    chore: define subject CPT template

---
+++ single-subject.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the subject custom post type.
+ *
+ * @package ArmandPhilippot-com
+ * @since 0.0.2
+ */
+
+get_header();
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		?>
+		<article class="page" itemscope itemtype="http://schema.org/BlogPosting">
+			<?php
+			get_template_part( 'template-parts/cpt/subject/header' );
+			get_template_part( 'template-parts/main/page-toc' );
+			get_template_part( 'template-parts/cpt/subject/content' );
+			?>
+			<aside class="page__aside page__aside--last">
+				<?php get_sidebar( 'pages' ); ?>
+			</aside>
+		</article>
+		<?php
+	}
+} else {
+	get_template_part( 'template-parts/main/none' );
+}
+get_footer();
