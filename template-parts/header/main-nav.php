113de40bb196ab5ad97199be15bec0643b6d0402
Armand Philippot <git@armandphilippot.com>
2021-04-12 12:25:26 +0200

    chore: handle case where nav menu is not defined
    This way the page menu is usable and look nice even if nav menu is not
    defined.

--- template-parts/header/main-nav.php
+++ template-parts/header/main-nav.php
@@ -9,15 +9,29 @@
 ?>
 <nav class="main-nav" aria-label="<?php esc_attr_e( 'Main', 'APCom' ); ?>">
 	<?php
-	wp_nav_menu(
-		array(
-			'menu_class'      => 'main-nav__list',
-			'container'       => '',
-			'theme_location'  => 'main-menu',
-			'item_spacing'    => 'discard',
-			'link_before'     => '<span>',
-			'link_after'      => '</span>',
-		)
-	);
+	if ( has_nav_menu( 'main-menu' ) ) {
+		wp_nav_menu(
+			array(
+				'menu_class'      => 'main-nav__list',
+				'container'       => '',
+				'theme_location'  => 'main-menu',
+				'item_spacing'    => 'discard',
+				'link_before'     => '<span>',
+				'link_after'      => '</span>',
+			)
+		);
+	} else {
+		wp_page_menu(
+			array(
+				'menu_class'      => 'main-nav__list main-nav__list--page-menu',
+				'container'       => 'ul',
+				'item_spacing'    => 'discard',
+				'link_before'     => '<span>',
+				'link_after'      => '</span>',
+				'before'          => '',
+				'after'           => '',
+			)
+		);
+	}
 	?>
 </nav>
