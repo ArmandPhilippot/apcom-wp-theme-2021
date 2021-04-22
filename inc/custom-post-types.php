d2d5c474be820a2564309ec29882f89274a2734c
Armand Philippot <git@armandphilippot.com>
2021-04-22 23:03:37 +0200

    chore: update permalinks after theme update
    After each update, permalinks are causing errors. It is maybe linked to
    the CPT and the `flush_rewrite_rules`.

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -413,3 +413,4 @@ function apcom_rewrite_flush() {
 	flush_rewrite_rules();
 }
 add_action( 'after_switch_theme', 'apcom_rewrite_flush' );
+add_action( 'upgrader_process_complete', 'apcom_rewrite_flush' );
