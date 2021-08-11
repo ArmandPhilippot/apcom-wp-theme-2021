ce2541e03e24b2a756a95769b026350f8e21ad1c
Armand Philippot <git@armandphilippot.com>
2021-08-11 11:37:28 +0200

    build: replace Gulp with webpack and npm-scripts
    So I changed some filenames and paths to adapt src & assets
    to webpack config.

---
+++ tools/webpack/webpack.common.js
@@ -0,0 +1,151 @@
+require( 'dotenv' ).config();
+
+const CopyPlugin = require( 'copy-webpack-plugin' );
+const DotenvWebpackPlugin = require( 'dotenv-webpack' );
+const ImageMinimizerPlugin = require( 'image-minimizer-webpack-plugin' );
+const { extendDefaultPlugins } = require( 'svgo' );
+const paths = require( './paths' );
+
+function recursiveIssuer( m, c ) {
+	const issuer = c.moduleGraph.getIssuer( m );
+
+	if ( issuer ) {
+		return recursiveIssuer( issuer, c );
+	}
+
+	const chunks = c.chunkGraph.getModuleChunks( m );
+
+	for ( const chunk of chunks ) {
+		return chunk.name;
+	}
+
+	return false;
+}
+
+module.exports = {
+	entry: {
+		scriptEditor: {
+			import: paths.src.scripts.editor,
+			filename: 'js/editor.js',
+		},
+		scriptFooter: {
+			import: paths.src.scripts.footer,
+			filename: 'js/footer.js',
+		},
+		scriptHeader: {
+			import: paths.src.scripts.header,
+			filename: 'js/header.js',
+		},
+		style: { import: paths.src.styles.style, filename: 'webpack/style.js' },
+		styleEditor: {
+			import: paths.src.styles.editor,
+			filename: 'webpack/editor-style.js',
+		},
+		stylePrint: {
+			import: paths.src.styles.print,
+			filename: 'webpack/print.js',
+		},
+	},
+	output: {
+		path: paths.dist,
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [ 'babel-loader' ],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'img/[name][ext]',
+				},
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: ( font ) => {
+						const relativePath = font.filename;
+						const filteredPath = relativePath.replace( 'src/', '' );
+						return filteredPath;
+					},
+				},
+			},
+		],
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				style: {
+					name: '../style',
+					test: ( m, c, entry = 'style' ) =>
+						m.constructor.name === 'CssModule' &&
+						recursiveIssuer( m, c ) === entry,
+					chunks: 'all',
+					enforce: true,
+				},
+				styleEditor: {
+					name: 'css/editor-style',
+					test: ( m, c, entry = 'styleEditor' ) =>
+						m.constructor.name === 'CssModule' &&
+						recursiveIssuer( m, c ) === entry,
+					chunks: 'all',
+					enforce: true,
+				},
+				stylePrint: {
+					name: 'css/print',
+					test: ( m, c, entry = 'stylePrint' ) =>
+						m.constructor.name === 'CssModule' &&
+						recursiveIssuer( m, c ) === entry,
+					chunks: 'all',
+					enforce: true,
+				},
+			},
+		},
+	},
+	plugins: [
+		new ImageMinimizerPlugin( {
+			minimizerOptions: {
+				plugins: [
+					[ 'gifsicle', { interlaced: true } ],
+					[ 'mozjpeg', { progressive: true, quality: 75 } ],
+					[ 'optipng', { optimizationLevel: 5 } ],
+					[
+						'svgo',
+						{
+							plugins: extendDefaultPlugins( [
+								{
+									name: 'removeTitle',
+									active: true,
+								},
+								{
+									name: 'removeViewBox',
+									active: false,
+								},
+								{
+									name: 'removeXMLNS',
+									active: false,
+								},
+							] ),
+						},
+					],
+				],
+			},
+		} ),
+		new CopyPlugin( {
+			patterns: [
+				{
+					from: paths.src.fonts,
+					to: 'fonts',
+					noErrorOnMissing: true,
+				},
+				{ from: paths.src.img, to: 'img', noErrorOnMissing: true },
+			],
+		} ),
+		new DotenvWebpackPlugin(),
+	],
+};
---
+++ tools/webpack/webpack.dev.js
@@ -0,0 +1,99 @@
+const paths = require( './paths' );
+
+require( 'dotenv' ).config();
+
+const protocol = process.env.WP_THEME_PROTOCOL;
+const host = process.env.WP_THEME_HOST;
+const port = process.env.WP_THEME_PORT;
+const contentFolder = process.env.WP_THEME_CONTENT_FOLDER;
+const themeFolder = process.env.WP_THEME_FOLDER;
+const siteURL = protocol + '://' + host;
+const publicPath =
+	siteURL +
+	':' +
+	port +
+	'/' +
+	contentFolder +
+	'/themes/' +
+	themeFolder +
+	'/assets/';
+
+const isHttps = protocol === 'https';
+
+module.exports = {
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/i,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2,
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [ 'autoprefixer' ],
+							},
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							implementation: require( 'sass' ),
+							sassOptions: {
+								indentWidth: 2,
+								outputStyle: 'expanded',
+								includePaths: paths.sassPaths,
+							},
+							sourceMap: true,
+						},
+					},
+				],
+			},
+		],
+	},
+	devtool: 'inline-source-map',
+	target: 'web',
+	devServer: {
+		host,
+		port,
+		publicPath,
+		contentBase: paths.contentBase,
+		watchContentBase: true,
+		https: ! isHttps
+			? false
+			: {
+				key: process.env.WP_THEME_HTTPS_KEY,
+				cert: process.env.WP_THEME_HTTPS_CERT,
+			},
+		proxy: {
+			'/': {
+				target: siteURL,
+				secure: false,
+				changeOrigin: true,
+				autoRewrite: true,
+			},
+		},
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods':
+				'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+			'Access-Control-Allow-Headers':
+				'X-Requested-With, content-type, Authorization',
+		},
+		overlay: {
+			warnings: true,
+			errors: true,
+		},
+		open: process.env.WP_THEME_OPEN,
+		writeToDisk: true,
+	},
+};
