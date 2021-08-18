6577403e33105f6fcb321ca00609af7cee0bfa7c
Armand Philippot <git@armandphilippot.com>
2021-08-18 16:30:42 +0200

    chore: add class to comment form labels and reorganize fields

--- inc/hooks/comments.php
+++ inc/hooks/comments.php
@@ -16,13 +16,15 @@
  */
 function apcom_comment_form_submit_button( $button ) {
 	$button =
-	'<button class="comment-form__btn btn btn--primary" type="submit"><span class="btn__body">' . __( 'Leave a comment', 'APCom' ) . '</span></button>';
+	'<button class="form__btn btn btn--primary" type="submit"><span class="btn__body">' . __( 'Leave a comment', 'APCom' ) . '</span></button>';
 	return $button;
 }
 add_filter( 'comment_form_submit_button', 'apcom_comment_form_submit_button' );
 
 /**
  * Add classes to comment reply link.
+ *
+ * @since  1.2.0
  *
  * @param string $link The HTML markup for the comment reply link.
  * @return string The modified link.
@@ -36,6 +38,8 @@ add_filter( 'comment_reply_link', 'apcom_comment_reply_link', 10, 1 );
 /**
  * Add classes to edit comment link.
  *
+ * @since  1.2.0
+ *
  * @param string $link The HTML markup for the edit comment link.
  * @return string The modified link.
  */
@@ -44,3 +48,34 @@ function apcom_edit_comment_link( $link ) {
 	return preg_replace( '/comment-edit-link/', 'comment-edit-link ' . $extra_classes, $link );
 }
 add_filter( 'edit_comment_link', 'apcom_edit_comment_link', 10, 1 );
+
+/**
+ * Change comment form fields order and add class to label.
+ *
+ * @param array $fields Default fields.
+ * @return string $fields Custom fields order with label classes.
+ *
+ * @since 1.2.0
+ */
+function apcom_comment_form_fields( $fields ) {
+	$label_classes = 'form__label';
+	$comment_field = preg_replace( '/<label /', '<label class="' . $label_classes . '" ', $fields['comment'] );
+	$author_field  = preg_replace( '/<label /', '<label class="' . $label_classes . '" ', $fields['author'] );
+	$email_field   = preg_replace( '/<label /', '<label class="' . $label_classes . '" ', $fields['email'] );
+	$url_field     = preg_replace( '/<label /', '<label class="' . $label_classes . '" ', $fields['url'] );
+
+	// Unset array fields.
+	unset( $fields['comment'] );
+	unset( $fields['author'] );
+	unset( $fields['email'] );
+	unset( $fields['url'] );
+
+	// Declare new order.
+	$fields['author']  = $author_field;
+	$fields['email']   = $email_field;
+	$fields['url']     = $url_field;
+	$fields['comment'] = $comment_field;
+
+	return $fields;
+}
+add_filter( 'comment_form_fields', 'apcom_comment_form_fields', 10, 1 );
