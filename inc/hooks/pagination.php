48871e9bbf9158a3fc1413819e01263adc563df0
Armand Philippot <git@armandphilippot.com>
2021-08-18 19:44:47 +0200

    chore: use custom classes for pagination

--- inc/hooks/pagination.php
+++ inc/hooks/pagination.php
@@ -27,3 +27,30 @@ function apcom_navigation_markup_template( $template, $class ) {
 	return $template;
 }
 add_filter( 'navigation_markup_template', 'apcom_navigation_markup_template', 10, 2 );
+
+/**
+ * Add classes to paginated links output.
+ *
+ * @param string $r HTML output.
+ * @return string $link The link with custom classes.
+ */
+function apcom_paginate_links_output( $r ) {
+	$list_classes      = 'pagination__list';
+	$items_classes     = 'pagination__item';
+	$current_classes   = 'pagination__link pagination__link--current';
+	$dots_classes      = 'pagination__link pagination__link--dots';
+	$prev_link_classes = 'pagination__link btn btn--secondary btn--previous';
+	$next_link_classes = 'pagination__link btn btn--secondary btn--next';
+	$links_classes     = 'pagination__link btn btn--secondary';
+
+	$r = preg_replace( '/<ul class=\'page-numbers\'>/', '<ul class="' . $list_classes . '">', $r );
+	$r = preg_replace( '/<li>/', '<li class="' . $items_classes . '">', $r );
+	$r = preg_replace( '/prev page-numbers/', $prev_link_classes, $r );
+	$r = preg_replace( '/next page-numbers/', $next_link_classes, $r );
+	$r = preg_replace( '/page-numbers current/', $current_classes, $r );
+	$r = preg_replace( '/page-numbers dots/', $dots_classes, $r );
+	$r = preg_replace( '/<a class="page-numbers/', '<a class="' . $links_classes, $r );
+
+	return $r;
+}
+add_filter( 'paginate_links_output', 'apcom_paginate_links_output', 10, 1 );
