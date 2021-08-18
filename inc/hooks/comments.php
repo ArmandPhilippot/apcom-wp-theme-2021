e598f373bc860df952d410a1089a4c32cd972ac4
Armand Philippot <git@armandphilippot.com>
2021-08-18 14:59:48 +0200

    chore: add classes to reply and edit comment links
    To avoid repeating styles inside Sass, I hook into the WP functions to
    add some classes.

--- inc/hooks/comments.php
+++ inc/hooks/comments.php
@@ -20,3 +20,27 @@ function apcom_comment_form_submit_button( $button ) {
 	return $button;
 }
 add_filter( 'comment_form_submit_button', 'apcom_comment_form_submit_button' );
+
+/**
+ * Add classes to comment reply link.
+ *
+ * @param string $link The HTML markup for the comment reply link.
+ * @return string The modified link.
+ */
+function apcom_comment_reply_link( $link ) {
+	$extra_classes = 'comment__link comment__link--reply btn btn--secondary';
+	return preg_replace( '/comment-reply-link/', 'comment-reply-link ' . $extra_classes, $link );
+}
+add_filter( 'comment_reply_link', 'apcom_comment_reply_link', 10, 1 );
+
+/**
+ * Add classes to edit comment link.
+ *
+ * @param string $link The HTML markup for the edit comment link.
+ * @return string The modified link.
+ */
+function apcom_edit_comment_link( $link ) {
+	$extra_classes = 'comment__link comment__link--edit btn btn--secondary';
+	return preg_replace( '/comment-edit-link/', 'comment-edit-link ' . $extra_classes, $link );
+}
+add_filter( 'edit_comment_link', 'apcom_edit_comment_link', 10, 1 );
