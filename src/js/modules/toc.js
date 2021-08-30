53e70e683f902ca3f522d8e6c4a0a8da70f37228
Armand Philippot <git@armandphilippot.com>
2021-08-30 19:16:27 +0200

    chore: check if page content exists before checking for headings

--- src/js/modules/toc.js
+++ src/js/modules/toc.js
@@ -5,10 +5,12 @@ const minimalistTOC = new TableOfContent();
 
 const addClassToc = () => {
 	const pageContent = document.getElementById( 'page__content' );
-	const headings = pageContent.getElementsByTagName( 'h2' );
 
-	if ( pageContent && headings.length > 0 ) {
-		pageContent.parentElement.classList.add( 'page--has-toc' );
+	if ( pageContent ) {
+		const headings = pageContent.getElementsByTagName( 'h2' );
+		if ( headings.length > 0 ) {
+			pageContent.parentElement.classList.add( 'page--has-toc' );
+		}
 	}
 };
 
